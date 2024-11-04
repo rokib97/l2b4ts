77149406674c92f379eba0c334b47ade10f4a78b
rokib97 <rokibulhasan.ph@gmail.com>
2024-11-05 04:17:08 +0600

    update practice

---
+++ practice/src /5.ts
@@ -0,0 +1,28 @@
+{
+  /**
+     * Task 5: Function Type
+    Objective: Write a function that reverses a string.
+
+    Instructions:
+
+    Write a function reverseString that:
+    Takes a single string argument.
+    Returns the string in reverse order.
+    Example:
+    Input: "hello"
+    Output: "olleh"
+
+     */
+
+  const reverseString = (str: string): string => {
+    // return str.split("").reverse().join("");
+    let myStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      myStr += str[i];
+    }
+    return myStr;
+  };
+
+  const result: string = reverseString("hello");
+  console.log(result);
+}
