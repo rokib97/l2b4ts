067e11eadec44f56ebca6b58945a0c6eae584db1
rokib97 <rokibulhasan.ph@gmail.com>
2025-01-15 12:48:45 +0600

    recaping ts basic data types

---
+++ ts-recap/module1/src/1.4.ts
@@ -0,0 +1,20 @@
+// basic data types
+const userName: string = "Rokib";
+const userId: number = 1244;
+const isAdmin: boolean = true;
+const x: undefined = undefined;
+const y: null = null;
+
+// any type
+let d;
+d = 123;
+d = "anything";
+
+// non primitive
+// array
+
+const names: string[] = ["sakib", "rakib", "tani"];
+const numbers: number[] = [1, 2, 4, 5, 69];
+
+// tuple
+const personDetails: [string, number, boolean] = ["rokib", 25, false];
